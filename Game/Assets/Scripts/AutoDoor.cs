0ac9fc3a81b46ed23892e88ef91851e80a1c3215
Cracked77 <100542493+Cracked77@users.noreply.github.com>
2022-05-27 13:29:35 +1000

    tried to make automatic door

---
+++ Game/Assets/Scripts/AutoDoor.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDoor : MonoBehaviour
+{
+    public GameObject movingDoor;
+    bool PlayerIsHere;
+    bool opening;
+    public float maximumOpening = 10f;
+    public float maximumClosing = 0f;
+
+    void Start()
+    {
+        PlayerIsHere = false;
+        opening = false;
+
+    }
+    void Update()
+    {
+        if (PlayerIsHere)
+        {
+            if (movingDoor.transform.position.x < maximumClosing)
+            {
+                movingDoor.transform.Translate(1f * Time.deltaTime, 0f, 0f);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            PlayerIsHere = true;
+        }
+
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            PlayerIsHere = false;
+        }
+    }
+}
