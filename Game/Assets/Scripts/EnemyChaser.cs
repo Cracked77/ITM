fe09a93f23b75a1a030b3b462af0dc85e1624b7d
Cracked77 <100542493+Cracked77@users.noreply.github.com>
2022-06-08 23:51:42 +1000

    added a bot

---
+++ Game/Assets/Scripts/EnemyChaser.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyChaser : MonoBehaviour
+{
+    private NavMeshAgent monster;
+    public GameObject FPS;
+    public float RunRange = 4.0f;
+ 
+    void Start()
+    {
+        monster = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, FPS.transform.position);
+
+        if (distance < RunRange)
+        {
+            Vector3 disToPlayer = transform.position - FPS.transform.position;
+            Vector3 newPosition = transform.position - disToPlayer;
+            monster.SetDestination(newPosition);
+        }
+    }
+}
